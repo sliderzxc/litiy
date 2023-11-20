f7ed4543d2f6df8682c5d94db92032b8c905642f
vadwwxz <vadwwxz@gmail.com>
2023-11-20 21:12:15 +0200

    fix MainActivity in sample

--- sample/src/main/kotlin/com/slidezxc/litiy/sample/MainActivity.kt
+++ sample/src/main/kotlin/com/slidezxc/litiy/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.slidezxc.litiy.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.slidezxc.wixy.samples.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
